9b6542643cc703b4ae706e2a9eea1e4caab15ae6
Andrew Dushane <adushane@truecar.com>
2018-11-14 15:20:04 -0500

    Fix prod build

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -9,41 +9,49 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "/dist/",
   },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    })
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
   ],
   optimization: {
-    minimize: true
+    minimize: true,
   },
   module: {
     rules: [
       {
-        test: /\.mdx?$/,
-        loader: "html-loader!markdown-loader?gfm=false"
+        test: /\.md$/,
+        loader: "html-loader!markdown-loader?gfm=false",
+      },
+      {
+        test: /\.mdx$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "babel-loader" },
+          { loader: require.resolve("./loader.js") },
+        ],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
+        loader: "style-loader!css-loader",
       },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: "url-loader?limit=8192"
+        loader: "url-loader",
       },
       {
         test: /\.svg$/,
-        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-      }
-    ]
-  }
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
+    ],
+  },
 };
